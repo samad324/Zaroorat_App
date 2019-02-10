7815455fb708ee0a898e8af60182afb4cefc2155
nomangul <nomangul2001@gmail.com>
2019-02-11 00:28:57 +0500

    changed: android and ios client ID's

--- app/src/config/google.config.js
+++ app/src/config/google.config.js
@@ -5,8 +5,8 @@
 */
 
 export const androidClientId =
+  "974050339093-o0684gs3sk0c32vdj1gq5hvm3va18f29.apps.googleusercontent.com";
+export const iosClientId =
+  "974050339093-cig4unnaq7cmj1tvknd5n0vu5lkatc81.apps.googleusercontent.com";
+export const androidStandaloneAppClientId =
   "316261621246-ad814hcipurpqi4di3df01lajt1ge40d.apps.googleusercontent.com";
-  export const iosClientId =
-  "316261621246-ge8apu10ihld6vicf3ovogag03h2f7vi.apps.googleusercontent.com";
-  export const androidStandaloneAppClientId =
-    "316261621246-ad814hcipurpqi4di3df01lajt1ge40d.apps.googleusercontent.com";
