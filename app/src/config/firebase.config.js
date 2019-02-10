9d5abae0fcc6d9c565cd5868091e06feedc5cc4a
nomangul <nomangul2001@gmail.com>
2019-02-11 00:30:53 +0500

    changed: firebase config

--- app/src/config/firebase.config.js
+++ app/src/config/firebase.config.js
@@ -4,11 +4,11 @@
 |--------------------------------------------------
 */
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyAKUpd8NoLFM7FfjCigHem8haBVOSVUyDI",
-  authDomain: "only4practice-b81ab.firebaseapp.com",
-  databaseURL: "https://only4practice-b81ab.firebaseio.com",
-  projectId: "only4practice-b81ab",
-  storageBucket: "only4practice-b81ab.appspot.com",
-  messagingSenderId: "316261621246"
+export const config = {
+  apiKey: "AIzaSyDMNcY8_6jANfrztPQB6KqbubDpEObRHWc",
+  authDomain: "nofikr-fyp.firebaseapp.com",
+  databaseURL: "https://nofikr-fyp.firebaseio.com",
+  projectId: "nofikr-fyp",
+  storageBucket: "nofikr-fyp.appspot.com",
+  messagingSenderId: "974050339093"
 };
