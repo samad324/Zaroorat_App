b38f6fe90b050d05f52177151f4dd4b485d83d02
nomangul <nomangul2001@gmail.com>
2019-02-18 14:05:08 +0500

    added: screen Stacks

--- app/src/navigation/drawer.navigator.js
+++ app/src/navigation/drawer.navigator.js
@@ -1,5 +1,17 @@
-import { createDrawerNavigator } from "react-navigation";
-import { AboutStack, HomeStack, ContactStack } from "../screens/screensConfig";
+import React from "react";
+import { View } from "react-native";
+import {
+  createDrawerNavigator,
+  SafeAreaView,
+  DrawerItems
+} from "react-navigation";
+import {
+  AboutStack,
+  HomeStack,
+  ContactStack,
+  ProfileStack
+} from "../screens/screensConfig";
+import { SignOut } from "../components/FacebookAuth";
 
 /**
 |--------------------------------------------------
@@ -7,8 +19,26 @@ import { AboutStack, HomeStack, ContactStack } from "../screens/screensConfig";
 |--------------------------------------------------
 */
 
-export const AppDrawerNavigator = createDrawerNavigator({
-  Home: { screen: HomeStack },
-  About: { screen: AboutStack },
-  Contact: { screen: ContactStack }
-});
+export const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: { screen: HomeStack },
+    About: { screen: AboutStack },
+    Contact: { screen: ContactStack },
+    Profile: { screen: ProfileStack }
+  },
+  {
+    contentComponent: props => (
+      <View style={{ flex: 1 }}>
+        <SafeAreaView
+          forceInset={{
+            top: "always",
+            horizontal: "never"
+          }}
+        >
+          <DrawerItems {...props} />
+          <SignOut />
+        </SafeAreaView>
+      </View>
+    )
+  }
+);
