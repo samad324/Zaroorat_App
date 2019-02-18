bb6a689dce8392c09d33adfcb856dc758fab0700
nomangul <nomangul2001@gmail.com>
2019-02-18 14:07:36 +0500

    removed: comments and firebase initilazation

--- app/App.js
+++ app/App.js
@@ -4,8 +4,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./src/store";
 import { AppNavigator } from "./src/navigation";
 import { Loading } from "./src/components/Loading";
-import { firebaseConfig } from "./src/config";
-import * as firebase from "firebase";
 
 /**
  |--------------------------------------------------
@@ -13,7 +11,18 @@ import * as firebase from "firebase";
  |--------------------------------------------------
  */
 
-firebase.initializeApp(firebaseConfig);
+// let db = firebase.firestore();
+// db.collection("users").add({
+//   first: "Ada",
+//   last: "Lovelace",
+//   born: 1815
+// })
+// .then(function(docRef) {
+//   console.log("Document written with ID: ", docRef.id);
+// })
+// .catch(function(error) {
+//   console.error("Error adding document: ", error);
+// });
 
 export default class App extends React.Component {
   render() {
