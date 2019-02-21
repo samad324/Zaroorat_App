171d58ec308d9d1ff67aab9f8dacad3f7b40ee1a
izaanjahangir <izaanjahangir2@gmail.com>
2019-02-22 04:51:07 +0500

    map marker updated

--- screens/MapScreen/MapScreen.js
+++ screens/MapScreen/MapScreen.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import MapView, { Marker } from "react-native-maps";
 import { fetchServicesForLocations } from "../../config/firebase";
 import { Styles } from "./Styles";
+import { Permissions, Contacts, Location } from "expo";
+import { calculateDistance } from '../../config/helpers';
 
 export default class MapScreen extends React.Component {
   static navigationOptions = {
@@ -13,48 +15,91 @@ export default class MapScreen extends React.Component {
     super(props);
 
     this.state = {
-      coordinates : []
+      coordinates: [],
+      currentLocation: {
+        latitude: 24.90479,
+        longitude: 67.0773317
+      }
     };
   }
+
+  getLocationAsync = async () => {
+    try {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+      if (status !== 'granted') {
+        alert("Ah! we can't access your location!");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+
+      const coords = {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      }
+
+      return coords;
+    }
+    catch (e) {
+      console.log("e =>", e);
+      throw e;
+    }
+  };
 
   async componentDidMount() {
     const { coordinates } = this.state;
     const arr = [];
-    const services = await fetchServicesForLocations();
-    for(let i=0; i< services.length; i++){
-      arr.push({...services[i].location, title: services[i].title});
+    try {
+      const services = await fetchServicesForLocations();
+      const currentLocation = await this.getLocationAsync();
+
+      for (let i = 0; i < services.length; i++) {
+        const distance = calculateDistance(currentLocation, services[i].location);
+        console.log("distance =>", distance);
+
+        if (distance <= 10) {
+          arr.push({ ...services[i].location, title: services[i].title });
+        }
+      }
+      this.setState({
+        coordinates: arr,
+        currentLocation
+      })
+      console.log('Arr', arr);
     }
-    this.setState({
-      coordinates : arr
-    })
-    console.log('Arr',arr);
+    catch (e) {
+      console.log(e)
+      alert("Error in Map screen")
+    }
   }
 
   render() {
-    const { coordinates } = this.state;
+    const { coordinates, currentLocation } = this.state;
+    
     return (
       <View style={Styles.container}>
         <MapView
           initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: currentLocation.latitude,
+            longitudeDelta: currentLocation.longitude
           }}
           style={{ flex: 1 }}
         >
-        {coordinates.map((value,index) => {
-          return (<MapView.Marker
-            coordinate={{
-              latitude : value.latitude,
-              longitude : value.longitude
-            }}
-            image={require("../../assets/images/map-icon.png")}
-            title={value.title}
-            style={{width : 20 , height : 20}}
-            key= {Math.random().toString()}
-          />)
-        })}
+          {coordinates.map((value, index) => {
+            return (<MapView.Marker
+              coordinate={{
+                latitude: value.latitude,
+                longitude: value.longitude
+              }}
+              image={require("../../assets/images/map-icon.png")}
+              title={value.title}
+              style={{ width: 20, height: 20 }}
+              key={Math.random().toString()}
+            />)
+          })}
         </MapView>
       </View>
     );
