4388bc4728b9457c6bd9531d9e551704937332cf
samad324889 <samad324889@gmail.com>
2019-02-22 05:08:43 +0500

    homeScreen tiles designed

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  ImageBackground,
   View
 } from "react-native";
 import { WebBrowser, Permissions, Location } from "expo";
@@ -15,7 +16,6 @@ import "firebase/firestore";
 
 import { setAllUsers } from "../../store/actions/authAction";
 import { styles } from "./Styles";
-import { MonoText } from "../../components/StyledText";
 import { setAllCategories } from "../../store/actions/generalAction";
 
 import { setUserLocation } from "../../config/firebase";
@@ -107,104 +107,36 @@ class HomeScreen extends React.Component {
   };
 
   render() {
+    const { allCategories } = this.props;
     return (
       <View style={styles.container}>
         <ScrollView
-          style={styles.container}
+          // style={styles.contentContainer}
           contentContainerStyle={styles.contentContainer}
         >
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require("../../assets/images/robot-dev.png")
-                  : require("../../assets/images/robot-prod.png")
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View
-              style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-            >
-              <MonoText style={styles.codeHighlightText}>
-                screens/HomeScreen.js
-              </MonoText>
-            </View>
-
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity
-              onPress={this._handleHelpPress}
-              style={styles.helpLink}
-            >
-              <Text style={styles.helpLinkText}>
-                Help, it didn’t automatically reload!
-              </Text>
-            </TouchableOpacity>
-          </View>
+          {allCategories.map((category, index) => {
+            return (
+              <View
+                style={[
+                  styles.tile,
+                  allCategories.length % 3 !== 0 &&
+                    allCategories.length - index <= 2 &&
+                    styles.extraSize
+                ]}
+                key={category.name}
+              >
+                <Image
+                  source={{ uri: category.thumbnail }}
+                  style={styles.tileImg}
+                />
+                <Text styles={styles.tileTxt}>{category.name}</Text>
+              </View>
+            );
+          })}
         </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>
-            This is a tab bar. You can edit it in:
-          </Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}
-          >
-            <MonoText style={styles.codeHighlightText}>
-              navigation/MainTabNavigator.js
-            </MonoText>
-          </View>
-        </View>
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use
-          useful development tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync(
-      "https://docs.expo.io/versions/latest/guides/development-mode"
-    );
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      "https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes"
-    );
-  };
 }
 
 const mapStateToProps = state => {
