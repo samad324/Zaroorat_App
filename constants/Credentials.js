a6f0838b96215eed7a4f4f1b8dd48de6eba49a27
samad324 <37283797+samad324@users.noreply.github.com>
2019-04-11 16:55:13 +0500

    updated

--- constants/Credentials.js
+++ constants/Credentials.js
@@ -1,13 +1,7 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDMNcY8_6jANfrztPQB6KqbubDpEObRHWc",
-  authDomain: "nofikr-fyp.firebaseapp.com",
-  databaseURL: "https://nofikr-fyp.firebaseio.com",
-  projectId: "nofikr-fyp",
-  storageBucket: "nofikr-fyp.appspot.com",
-  messagingSenderId: "974050339093"
+firebase_config_OBJ
 };
 
 export const facebookConfig = {
-  appId: "829648040718922",
-  appSecret: "55385f336618cce71ad9ab25984c24fc"
+  appId: "FB_API_KEY"
 };
